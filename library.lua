ef215c2068dd4c751b8f76fe530353175e2d4380
Ad <95490130+ad-devlpr@users.noreply.github.com>
2024-06-22 14:11:00 +0530

    Update library.lua

--- library.lua
+++ library.lua
@@ -90,7 +90,7 @@ local Input_Roundify_4px = Instance.new("ImageLabel")
 local Windows = Instance.new("Frame")
 
 imgui.Name = "imgui"
-if not (getgenv().compatabilitymode) then imgui.Parent = game:GetService("CoreGui") end
+if compatmode == false then imgui.Parent = game:GetService("CoreGui") end
 
 Prefabs.Name = "Prefabs"
 Prefabs.Parent = imgui
